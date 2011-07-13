39786523cdce659ebc7c30d1fba1ad64984d592b
Benjamin Pavie <benjamin.pavie@utsouthwestern.edu>
2011-07-13 16:09:46 -0500

    Test Commit

--- gui.m
+++ gui.m
@@ -18,7 +18,7 @@ function varargout = gui(varargin)
 %      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
 %      instance to run (singleton)".
 %
-% See also: GUIDE, GUIDATA, GUIHANDLES
+% See also: GUIDE, GUIDATA, GUIHANDLES 
  
 % Edit the above text to modify the response to help gui
 
